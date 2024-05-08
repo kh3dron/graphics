de52312e6dda89e2acb482c31bbc14f62b8cb566
Tristan Saldanha <tristansaldanha@gmail.com>
2024-05-08 15:41:04 -0700

    more

---
+++ webgl/HelloPoint2.js
@@ -0,0 +1,43 @@
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'attribute float a_PointSize;\n' + 
+    'void main() {\n' +
+    ' gl_Position = a_Position;\n' +
+    ' gl_PointSize = a_PointSize;\n' +
+    '}\n';
+
+var FSHADER_SOURCE =
+    'void main() {\n' +
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+    '}\n';
+
+function main() {
+
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+
+    if (a_Position < 0) {
+        console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    // Pass vertex position to attribute variable
+    gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0); // 3f indicates 3 float values
+    gl.vertexAttrib1f(a_PointSize, 5.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}

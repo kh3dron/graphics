f5692d837ef067e74d65bd5d0fa278f6712fc0a7
Tristan Saldanha <tristansaldanha@gmail.com>
2024-05-09 11:31:07 -0700

    content

--- webgl/ClickedPoint.js
+++ webgl/ClickedPoint.js
@@ -35,8 +35,9 @@ function main() {
         console.log('Failed to get the storage location of a_Position');
         return;
     }
-
-    canvas.onmousedown = function (ev) {
+    
+    // anonymous function, prevents global variable pollution
+    canvas.onmousedown = function (ev) { 
         click(ev, gl, canvas, a_Position);
     };
 
@@ -46,19 +47,23 @@ function main() {
 
 var g_points = [];
 function click(ev, gl, canvas, a_Position) {
-    var x = ev.clientX;
+    var x = ev.clientX; 
     var y = ev.clientY;
     var rect = ev.target.getBoundingClientRect();
+    
+    // client area != canvas area != webgl area
+    // canvas origin is at the top-left corner, while webgl origin is at the center
 
     x = ((x - rect.left) - canvas.width / 2) / (canvas.width / 2);
     y = (canvas.height / 2 - (y - rect.top)) / (canvas.height / 2);
-    g_points.push(x); g_points.push(y);
+    g_points.push([x, y]);
 
     gl.clear(gl.COLOR_BUFFER_BIT);
     var len = g_points.length;
 
-    for (var i = 0; i < len; i += 2) {
-        gl.vertexAttrib3f(a_Position, g_points[i], g_points[i + 1], 0.0);
+    for (var i = 0; i < len; i += 1) {
+        var xy = g_points[i];
+        gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
         gl.drawArrays(gl.POINTS, 0, 1);
     }
 }

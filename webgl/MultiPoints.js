d500c10e5a9c5bc1297ac969cfae016be81e8927
Tristan Saldanha <tristansaldanha@gmail.com>
2024-05-09 16:01:50 -0700

    multipoints

---
+++ webgl/MultiPoints.js
@@ -0,0 +1,66 @@
+// ClickedPoint.js
+// Vertex shader program
+
+var VSHADER_SOURCE =
+    'attribute vec4 a_Position;\n' +
+    'void main() {\n' +
+    ' gl_Position = a_Position;\n' +
+    ' gl_PointSize = 10.0;\n' +
+    '}\n';
+
+// Fragment shader program
+var FSHADER_SOURCE =
+    'precision mediump float;\n' + 
+    'uniform vec4 u_FragColor;\n' + // uniform variable
+    'void main() {\n' +
+    ' gl_FragColor = u_FragColor;\n' +
+    '}\n';
+
+function main() {
+
+    var canvas = document.getElementById('webgl');
+    var gl = getWebGLContext(canvas);
+
+    if (!gl) {
+        console.log('Failed to get the rendering context for WebGL');
+        return;
+    }
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initialize shaders.');
+        return;
+    }
+
+    var n = initVertexBuffers(gl);
+    if (n < 0) {
+        console.log('Failed to set the positions of the vertices');
+        return;
+    }
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, n);
+
+}
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([
+        0.0, 0.5, -0.5, -0.5, .5, -0.5
+    ]);
+    var n = 3;
+
+    // Create a buffer object
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log('Failse to create the buffer object ');
+        return -1;
+    }
+
+    // bind the buffer object to target
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+    return n;
+}
+

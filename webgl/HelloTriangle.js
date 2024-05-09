5ef64c31277f0e365715b2ad8313be54b60ed32d
Tristan Saldanha <tristansaldanha@gmail.com>
2024-05-09 16:35:08 -0700

    triangle

--- webgl/HelloTriangle.js
+++ webgl/HelloTriangle.js
@@ -5,15 +5,12 @@ var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'void main() {\n' +
     ' gl_Position = a_Position;\n' +
-    ' gl_PointSize = 10.0;\n' +
     '}\n';
 
 // Fragment shader program
 var FSHADER_SOURCE =
-    'precision mediump float;\n' + 
-    'uniform vec4 u_FragColor;\n' + // uniform variable
     'void main() {\n' +
-    ' gl_FragColor = u_FragColor;\n' +
+    ' gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
     '}\n';
 
 function main() {
